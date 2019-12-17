362c7faf7a648e30e8287df9ba637dfdeb412caf
Ashish Prabhune <ashuprabhune@gmail.com>
2019-12-17 15:07:54 -0500

    1. Added anotations 2. Enabled H2 console property

--- src/main/java/guru/springframework/spring5webapp/model/Author.java
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -1,14 +1,30 @@
 package guru.springframework.spring5webapp.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
-
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.AUTO)
+    public Long id;
     private String firstName;
     private String lastName;
 
-
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> authors =  new HashSet<>();
+
+    public Author() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public Author(String firstName, String lastName) {
         this.firstName = firstName;
@@ -36,9 +52,6 @@ public class Author {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
-
-
 
 
 }
--- src/main/java/guru/springframework/spring5webapp/model/Book.java
+++ src/main/java/guru/springframework/spring5webapp/model/Book.java
@@ -1,15 +1,23 @@
 package guru.springframework.spring5webapp.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
-
+@Entity
 public class Book {
-    private Long id;
+    @Id
+    @GeneratedValue( strategy = GenerationType.AUTO)
+    public Long id;
     private String title;
     private String isbn;
     private String publisher;
 
+    @ManyToMany
+    @JoinTable(name="author_book" ,joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors =  new HashSet<>();
+
+    public Book() {
+    }
 
     public Book(Long id, String title, String isbn, String publisher) {
         this.id = id;
