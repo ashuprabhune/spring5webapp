512f1eba87b4d443775fc98f02d60523181a2cd2
Ashish Prabhune <ashuprabhune@gmail.com>
2019-12-17 19:00:03 -0500

    Added publisher entity

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repository.AuthorRepository;
 import guru.springframework.spring5webapp.repository.BookRepository;
+import guru.springframework.spring5webapp.repository.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -12,10 +14,13 @@ import org.springframework.stereotype.Component;
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
     AuthorRepository authorRepository;
     BookRepository bookRepository;
+    PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+
     }
 
     @Override
@@ -25,9 +30,14 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private void initData(){
 
+        //Alisha the publisher
+        Publisher alisha = new Publisher("Alisha Lohan","Chandigarh","Hryana");
+        publisherRepository.save(alisha);
+
         //Abhishek
         Author abhi = new Author("Abhishek","Bagde");
-        Book aaa =  new Book("How to waste time playing CS","1234","Alisha Lohan");
+        Book aaa =  new Book("How to waste time playing CS","1234",alisha);
+
         abhi.getBooks().add(aaa);
         aaa.getAuthors().add(abhi);
 
@@ -36,7 +46,7 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
         //Chirag
         Author chirag = new Author("Chirag","Mali");
-        Book ccc =  new Book("How to spend money unwisely","4321","Alisha Lohan");
+        Book ccc =  new Book("How to spend money unwisely","4321",alisha);
         chirag.getBooks().add(ccc);
         ccc.getAuthors().add(chirag);
 
